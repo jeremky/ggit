dd1a7eaf4b98db8fe73c96e94e763335bb5e49b8
JeremKy <jeremky@me.com>
2025-01-03 23:56:48 +0100

    Mise à jour

--- ggit.sh
+++ ggit.sh
@@ -1,37 +1,35 @@
 #!/bin/bash
 
-## Variables
-cfg="$(dirname "$0")/ggit.cfg"
-
-## Verification
+## Verification des droits
 if [ "$USER" = "root" ] ; then
   echo "Ne pas lancer en tant que root !"
   exit 0
 fi
-if [ -f $cfg ] ; then
-  . $cfg
-else
-  echo "Fichier $cfg introuvable"
-  exit 0
-fi
 
 ## Commandes
-echo ""
-for gd in $(ls $gitdir) ; do
-  if [ -d $gitdir/$gd/.git ] ; then
-    echo "====> Dossier $gd :"
-    cd $gitdir/$gd
-    case $1 in
-      p|pull)
-        git pull
-        ;;
-      *|push)
-        git add *
-        git commit -m "Mise à jour"
-        git push
-        ;;
-    esac
-    echo ""
-  fi
-done
-cd $HOME
+cfg="$(dirname "$0")/ggit.cfg"
+if [ ! -f $cfg ] ; then
+  echo "Fichier $cfg introuvable"
+  exit 0
+else
+  . $cfg
+  echo ""
+  for gd in $(ls $gitdir) ; do
+    if [ -d $gitdir/$gd/.git ] ; then
+      echo "====> Dossier $gd :"
+      cd $gitdir/$gd
+      case $1 in
+        p|pull)
+          git pull
+          ;;
+        *|push)
+          git add *
+          git commit -m "Mise à jour"
+          git push
+          ;;
+      esac
+      echo ""
+    fi
+  done
+  cd $HOME
+fi
