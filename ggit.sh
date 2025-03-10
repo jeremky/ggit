822fb079702dd5b798a229852d0e1d8eaa965752
JeremKy <jeremky@me.com>
2025-03-10 21:27:11 +0100

    Mise à jour

--- ggit.sh
+++ ggit.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-if [ "$USER" = "root" ] ; then
+if [[ "$USER" = "root" ]]; then
   echo "Ne pas lancer en tant que root !"
   exit 0
 fi
 
 cfg="$(dirname "$0")/ggit.cfg"
-if [ ! -f $cfg ] ; then
+if [[ ! -f $cfg ]]; then
   echo "Fichier $cfg introuvable"
   exit 0
 else
   . $cfg
   echo ""
   for gd in $(ls -1 $gitdir) ; do
-    if [ -d $gitdir/$gd/.git ] ; then
+    if [[ -d $gitdir/$gd/.git ]]; then
       echo "====> Dossier $gd :"
       cd $gitdir/$gd
       case $1 in
