ae5271edacba08a813819175ab520ddd3d89b7a3
JeremKy <jeremky@me.com>
2025-06-15 18:21:49 +0200

    Mise à jour

--- ggit.sh
+++ ggit.sh
@@ -1,38 +1,32 @@
 #!/bin/bash
 
-cecho() {
-  local RESET='\033[0m'
-  case $1 in
-    m|message) color='\033[0;32m' ;;
-    w|warning) color='\033[0;33m' ;;
-    e|error)   color='\033[0;31m' ;;
-  esac
-  shift
-  echo -e "${color}$*${RESET}"
-}
+# Messages colorisés
+error()    { echo -e "\033[0;31m$*\033[0m"; }
+message()  { echo -e "\033[0;32m$*\033[0m"; }
+warning()  { echo -e "\033[0;33m$*\033[0m"; }
 
 # Droits
 if [[ "$USER" = "root" ]]; then
-  cecho e "Ne pas lancer en tant que root !"
+  error "Ne pas lancer en tant que root !"
   exit 0
 fi
 
 gpush() { 
   echo ""
-  cecho w "====> push de $(basename "$(realpath .)")"
+  warning "====> push de $(basename "$(realpath .)")"
   git add * ; git add .* ; git commit -m "$message" ; git push
 }
 
 gpull() {
   echo ""
-  cecho m "====> pull de $(basename "$(realpath .)")"
+  message "====> pull de $(basename "$(realpath .)")"
   git pull
 }
 
 # Exécution
 cfg="$(dirname "$0")/ggit.cfg"
 if [[ ! -f $cfg ]]; then
-  cecho e "Fichier $cfg introuvable"
+  error "Fichier $cfg introuvable"
   exit 0
 else
   . $cfg
