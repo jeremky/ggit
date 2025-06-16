24e096b776f66c361c293b72e6ace41405fdfff2
JeremKy <jeremky@me.com>
2025-06-16 16:43:31 +0200

    Mise à jour

--- ggit.sh
+++ ggit.sh
@@ -2,21 +2,23 @@
 
 # Messages colorisés
 error()    { echo -e "\033[0;31m====> $*\033[0m" ;}
-message()  { echo -e "\033[0;32m====> $*\033[0m" ; echo "" ;}
-warning()  { echo "" ; echo -e "\033[0;33m====> $*\033[0m" ;}
+message()  { echo -e "\033[0;32m====> $*\033[0m" ;}
+warning()  { echo -e "\033[0;33m====> $*\033[0m" ;}
 
 # Droits
 if [[ "$USER" = "root" ]]; then
   error "Ne pas lancer en tant que root !"
-  exit 0
+  exit 1
 fi
 
 gpush() { 
+  echo ""
   warning "push de $(basename "$(realpath .)")"
   git add * ; git add .* ; git commit -m "$message" ; git push
 }
 
 gpull() {
+  echo ""
   warning "pull de $(basename "$(realpath .)")"
   git pull
 }
@@ -25,7 +27,7 @@ gpull() {
 cfg="$(dirname "$0")/ggit.cfg"
 if [[ ! -f $cfg ]]; then
   error "Fichier $cfg introuvable"
-  exit 0
+  exit 1
 else
   . $cfg
   case $1 in
@@ -54,6 +56,5 @@ else
       fi
   esac
   echo ""
-  message "Synchronisation git terminée"
   cd $HOME
 fi
