8b9fdcd39f634ce03943f62bc2e72b7b38a79447
JeremKy <jeremky@me.com>
2025-06-23 12:52:15 +0200

    Mise à jour

--- ggit.sh
+++ ggit.sh
@@ -15,7 +15,6 @@ gpush() {
   echo
   warning "push de $(basename "$(realpath .)")"
   git add $(git ls-files --exclude-standard; git ls-files --modified; git ls-files --others --exclude-standard)
-  #git add * ; git add .* 
   git commit -m "$message" ; git push
 }
 
